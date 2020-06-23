5a3a70a1fd974f500f52f22f9d1bee0b393056c1
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-06-23 17:09:56 -0400

    Adicionado todas as funções de filtros pra mostrar o país juntamente com sua população

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Countries from './components/countries/Countries';
+import Header from './components/header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      allCountries: [],
+      filteredCountries: [],
+      filteredPopulation: 0,
+      filter: '',
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    
+    const json = await res.json();
+    
+    const allCountries = json.map(({ name, numericCode, flag, population }) => {
+      return {
+        id: numericCode,
+        name,
+        filterName: name.toLowerCase(),
+        flag,
+        population,
+      };
+    });
+
+    const filteredPopulation = this.calculateTotalPopulationFrom(allCountries);
+
+    this.setState({
+      allCountries,
+      filteredCountries: Object.assign([], allCountries),
+      filteredPopulation,
+    });
+  }
+
+  calculateTotalPopulationFrom = (countries) => {
+    const totalPopulation = countries.reduce((accumulator, current) => {
+      return accumulator + current.population;
+    }, 0);
+    return totalPopulation;
+  };
+
+  handleChangeFilter = (newText) => {
+    this.setState({
+      filter: newText,
+    });
+
+    const filterLowerCase = newText.toLowerCase();
+    
+    const filteredCountries = this.state.allCountries.filter((country) => {
+      return country.filterName.includes(filterLowerCase);
+    });
+
+    const filteredPopulation = this.calculateTotalPopulationFrom(filteredCountries);
+
+    this.setState({
+      filteredCountries,
+      filteredPopulation,
+    });
+  };
+
+  render() {
+    const { filteredCountries, filter, filteredPopulation } = this.state;
+
+      return (
+        <div className="container">
+          <h1 style={styles.centeredTitle}>React Countries</h1>
+
+          <Header
+            filter={filter}
+            countryCount={filteredCountries.length}
+            totalPopulation={filteredPopulation}
+            onChangeFilter={this.handleChangeFilter}
+          />
+
+          <Countries countries={filteredCountries} />
+        </div>
+    );
+  }
 }
 
-export default App;
+const styles = {
+  centeredTitle: {
+    textAlign: 'center',
+  },
+};
